6514183e7680960f2d3d4eb86e9e3f4442537bdd
Reinoptland <reinoptland@gmail.com>
2020-03-12 17:57:36 +0100

    Make selector for developer use in component

--- src/pages/DeveloperDetails/index.js
+++ src/pages/DeveloperDetails/index.js
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchDeveloperById } from "../../store/developerDetails/actions";
+import { selectDeveloperDetails } from "../../store/developerDetails/selectors";
 
 export default function DeveloperDetails() {
   const dispatch = useDispatch();
   const { id } = useParams();
+  const developer = useSelector(selectDeveloperDetails);
 
   console.log("DEVELOPER ID", id);
+  console.log("DEVELOPER", developer);
 
   useEffect(() => {
     dispatch(fetchDeveloperById(id));
