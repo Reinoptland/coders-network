c25bc2e51f6856b2ffec784d5901dd2511bfca38
Reinoptland <reinoptland@gmail.com>
2020-03-12 10:30:08 +0100

    Add route for homepage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./App.css";
-import Jumbotron from "react-bootstrap/Jumbotron";
 import PostsList from "./pages/PostsList";
+import Home from "./pages/Home";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Jumbotron>
-        <h1>Hello, world!</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
-      </Jumbotron>
       <Switch>
+        <Route exact path="/" component={Home} />
         <Route path="/posts" component={PostsList} />
       </Switch>
     </div>
