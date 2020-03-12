13006b18e211441c55a6ffc4d327275668a9fae9
Reinoptland <reinoptland@gmail.com>
2020-03-12 16:05:28 +0100

    Post details reducer & handle action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import posts from "./posts/reducer";
+import postDetails from "./postDetails/reducer";
 import developers from "./developers/reducer";
 
 const reducer = combineReducers({
   posts: posts,
+  postsDetails: postDetails,
   developers: developers
 });
 export default reducer;
