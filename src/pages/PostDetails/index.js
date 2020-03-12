0c71813fe967aa593a7872b94bb969ed9a0bacb9
Reinoptland <reinoptland@gmail.com>
2020-03-12 16:17:59 +0100

    Display post & comments

--- src/pages/PostDetails/index.js
+++ src/pages/PostDetails/index.js
@@ -3,6 +3,11 @@ import { useParams } from "react-router-dom";
 import { fetchPostById } from "../../store/posts/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { selectPostDetails } from "../../store/postDetails/selectors";
+import ReactMarkdown from "react-markdown";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Spinner from "react-bootstrap/Spinner";
+import Card from "react-bootstrap/Card";
 
 export default function PostDetails() {
   // I WANT THE ID OF THIS POST
@@ -16,5 +21,23 @@ export default function PostDetails() {
     dispatch(fetchPostById(params.id));
   }, [dispatch, params.id]);
 
-  return <div>DETAILS</div>;
+  if (!postDetails.title) return <Spinner />;
+  return (
+    <Container>
+      <Jumbotron>
+        <h1>{postDetails.title}</h1>
+        <h3>Author: {postDetails.developer.name}</h3>
+      </Jumbotron>
+      <ReactMarkdown source={postDetails.content} />
+      {postDetails.comments.map(comment => {
+        console.log(comment);
+        return (
+          <Card>
+            <h5>{comment.developer.name} says:</h5>
+            <p>{comment.text}</p>
+          </Card>
+        );
+      })}
+    </Container>
+  );
 }
