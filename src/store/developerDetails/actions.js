58ac269b5ca323573d61e9dfbf7feeb0ef1b220c
Reinoptland <reinoptland@gmail.com>
2020-03-12 17:53:25 +0100

    Dispatch action on success

--- src/store/developerDetails/actions.js
+++ src/store/developerDetails/actions.js
@@ -1,10 +1,19 @@
 import axios from "axios";
 
+function developerFetchedSuccess(developer) {
+  return {
+    type: "FETCH_DEVELOPER_DETAILS_SUCCESS",
+    payload: developer
+  };
+}
+
 export function fetchDeveloperById(developerId) {
   return async function(dispatch, getState) {
     const response = await axios.get(
       `https://codaisseur-coders-network.herokuapp.com/developers/${developerId}`
     );
     console.log(response);
+
+    dispatch(developerFetchedSuccess(response.data));
   };
 }
