696d79c8fd07a2b0a2239c30d2ad03604610f7ef
Reinoptland <reinoptland@gmail.com>
2020-03-13 08:43:10 +0100

    Hardcode limit 1 & offset 0 when fetching developers

--- src/store/developers/actions.js
+++ src/store/developers/actions.js
@@ -10,7 +10,7 @@ function fetchDevelopersSuccess(data) {
 export function fetchDevelopersThunk() {
   return async function(dispatch, getState) {
     const response = await axios.get(
-      "https://codaisseur-coders-network.herokuapp.com/developers"
+      "https://codaisseur-coders-network.herokuapp.com/developers?offset=0&limit=1"
     );
 
     const action = fetchDevelopersSuccess(response.data);
