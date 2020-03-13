e4daf1008411ef3525046aa35386f1d1aebbfaba
Reinoptland <reinoptland@gmail.com>
2020-03-13 14:46:57 +0100

    Put token in storage when authenticated, use in initial State

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -1,10 +1,11 @@
 const initialState = {
-  token: null
+  token: localStorage.getItem("token") || null
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case "USER_AUTHENTICATED":
+      localStorage.setItem("token", action.payload);
       return { ...state, token: action.payload };
 
     case "PROFILE_FETCHED":
