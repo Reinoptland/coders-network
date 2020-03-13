40576ddcbf51b9aa60a2159d7353a5a561185c5c
Reinoptland <reinoptland@gmail.com>
2020-03-13 10:33:47 +0100

    Send post request with data to api to create a dev

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,8 +1,18 @@
 import axios from "axios";
 
 export function signUpThunk(name, password, email) {
-  return function(dispatch, getState) {
+  return async function(dispatch, getState) {
     console.log("INSIDE THUNK", name, password, email);
-    // axios.post()
+    const response = await axios.post(
+      "https://codaisseur-coders-network.herokuapp.com/signup",
+      // send request body as a second argument, just an object with the data
+      {
+        name: name,
+        email: email,
+        password: password
+      }
+    );
+
+    console.log(response);
   };
 }
