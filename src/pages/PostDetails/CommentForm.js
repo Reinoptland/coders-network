6892f24e76581bb52e46af9624570c613fc22ed5
Reinoptland <reinoptland@gmail.com>
2020-03-13 15:42:45 +0100

    Event listeners for comment form

--- src/pages/PostDetails/CommentForm.js
+++ src/pages/PostDetails/CommentForm.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export default function CommentForm() {
+  const [text, setText] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(text);
+  }
+
   return (
     <div>
-      <Form>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group
+          value={text}
+          onChange={event => setText(event.target.value)}
+          controlId="exampleForm.ControlTextarea1"
+        >
           <Form.Label>
             <h2>Post A Comment</h2>
           </Form.Label>
