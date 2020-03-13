d993b42c85dd9a0b9ab833179cdc6f9a361f8423
Reinoptland <reinoptland@gmail.com>
2020-03-13 10:36:40 +0100

    Dispatch action on successfull signup

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+function signUpSuccess(token) {
+  return {
+    type: "SIGN_UP_SUCCESS",
+    payload: token
+  };
+}
+
 export function signUpThunk(name, password, email) {
   return async function(dispatch, getState) {
     console.log("INSIDE THUNK", name, password, email);
@@ -13,6 +20,8 @@ export function signUpThunk(name, password, email) {
       }
     );
 
-    console.log(response);
+    console.log(response.data.jwt);
+
+    dispatch(signUpSuccess(response.data.jwt));
   };
 }
