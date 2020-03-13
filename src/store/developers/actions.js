2830400ff208dcbdec239e26a4cdcc1dc18d4764
Reinoptland <reinoptland@gmail.com>
2020-03-13 08:44:54 +0100

    Check how many developers are in the state in action

--- src/store/developers/actions.js
+++ src/store/developers/actions.js
@@ -9,6 +9,12 @@ function fetchDevelopersSuccess(data) {
 
 export function fetchDevelopersThunk() {
   return async function(dispatch, getState) {
+    const state = getState();
+    console.log(state.developers.rows);
+
+    const developerCount = state.developers.rows.length;
+    console.log(developerCount);
+
     const response = await axios.get(
       "https://codaisseur-coders-network.herokuapp.com/developers?offset=0&limit=1"
     );
